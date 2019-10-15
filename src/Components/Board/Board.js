027a1a9b689e4a84ac705c06ea8c2aaff87280cd
Jonathan Omarwoth <oma0256@gmail.com>
2019-10-15 20:07:08 +0300

    Generate board based on width and height

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -2,26 +2,30 @@ import React, { Component } from 'react';
 import './Board.css';
 
 class Board extends Component {
+  state = {
+    row: null,
+    column: null,
+  };
+
+  componentDidMount() {
+    const row = prompt('Please enter the board height: ');
+    const column = prompt('Please enter the board width: ');
+    this.setState({ row: +row, column: +column });
+  }
+
   render() {
+    const { row, column } = this.state;
+    const board = [];
+    for (let i = 0; i < row; i++) {
+      const rowCells = [];
+      for (let j = 0; j < column; j++) {
+        rowCells.push(<td key={j} className='cell'></td>);
+      }
+      board.push(<tr key={i}>{rowCells}</tr>);
+    }
     return (
       <table className='table'>
-        <tbody>
-          <tr>
-            <td className='cell'></td>
-            <td className='cell'></td>
-            <td className='cell'></td>
-          </tr>
-          <tr>
-            <td className='cell'></td>
-            <td className='cell'></td>
-            <td className='cell'></td>
-          </tr>
-          <tr>
-            <td className='cell'></td>
-            <td className='cell'></td>
-            <td className='cell'></td>
-          </tr>
-        </tbody>
+        <tbody>{board}</tbody>
       </table>
     );
   }
